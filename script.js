700cb633e6a09b5100578e804d98254b3d9366b1
AirlanRubens <sutilartest@gmail.com>
2025-04-16 13:32:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function baixarPDF() {
+  alert("Você pode usar bibliotecas como jsPDF para exportar em PDF. Esta função é um exemplo!");
+  // Exemplo com jsPDF:
+  // const doc = new jsPDF();
+  // doc.text("Seu Nome - Currículo", 10, 10);
+  // doc.save("curriculo.pdf");
+}
